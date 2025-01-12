ceb9c0947741c4232e24825d8f3e253d9e1fc892
lordsainath <lordsainath@gmail.com>
2025-01-12 19:58:10 +0530

    Executed some Array Methods

---
+++ Array/index.js
@@ -0,0 +1,14 @@
+// array is a collection of elemtetns used to store mulitple values in a single variable it inlcudes number, string , objects, or other data types.array is index based so index start from 0 which means array first elemnet index is 0;it follows insertio order.array are dynamic can change in size
+
+// Declaraing in
+// by using literal notation
+var arr1 = [1,2,3,4,5];
+console.log(arr1)
+arr1.length
+
+// by using new keyword
+let arr2= new Array("a", "b", "c");
+
+let arr3 = new Array(5);
+
+
