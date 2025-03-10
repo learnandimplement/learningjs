b7d565cf9d905eff497916452846f747dd48f637
lordsainath <lordsainath8@gmail.com>
2025-03-10 23:36:00 +0530

    Namaste Js

---
+++ NJ/index.js
@@ -0,0 +1,46 @@
+
+// Hoisting and execution Context
+// var x = 7;
+
+// function getName(){
+//     console.log("hello Javascript");
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+// ----------------------------------------------------
+
+// function invocation and varible environment
+// var x = 10;
+
+// function a(){
+//     var x = 15;
+//     console.log(x);
+// }
+
+// function b(){
+ 
+//     console.log(x);
+// }
+
+// console.log(x);
+// a();
+// b();
+
+// console.log(x);
+
+
+
+// glboal object window
+var a;
+
+
+
+{
+    var a = 5;
+    let b = 10;
+    const c = 100;
+}
